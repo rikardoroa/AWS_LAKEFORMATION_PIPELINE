629ffea2a2adfc6fbb96b7cdbcca78b1425adc54
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-31 02:54:34 -0500

    Update scheduler.tf

--- aws_resources/eventbridge_module/scheduler.tf
+++ aws_resources/eventbridge_module/scheduler.tf
@@ -1,20 +1,3 @@
-# resource "aws_scheduler_schedule" "lambda_api_coinbase_scheduler" {
-#   name       = "lambda_api_coinbase_scheduler"
-#   group_name = "default"
-
-#   flexible_time_window {
-#     mode = "OFF"
-#   }
-
-#   schedule_expression = "rate(5 minutes)"
-
-#   target {
-#     arn      = var.lambda_arn
-#     role_arn = var.lambda_role
-#   }
-# }
-
-
 # --- IAM Role for EventBridge Scheduler ---
 resource "aws_iam_role" "scheduler_invoke_role" {
   name = "iam_eventbridge_scheduler_invoke_lambda"
