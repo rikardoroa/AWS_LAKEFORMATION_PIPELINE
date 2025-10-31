e2a5449c1a558cacb3b28961c1d34cc139b05461
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-31 00:15:53 -0500

    permissions

--- aws_resources/lambda_module/iam.tf
+++ aws_resources/lambda_module/iam.tf
@@ -152,7 +152,12 @@
 #   source_arn    = var.scheduler_arn
 # }
 
-# inner account id and region
+
+##############################################
+# IAM ROLE & POLICY FOR LAMBDA (Coinbase API)
+##############################################
+
+# Obtener cuenta y región actual
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
@@ -179,10 +184,15 @@ resource "aws_iam_role" "iam_dev_role_cb_api" {
   })
 }
 
-# Lambda Policy Document
+#################################################
+# IAM POLICY DOCUMENT - Lambda Permissions
+#################################################
+
 data "aws_iam_policy_document" "pipeline_dev_policy_cb_api" {
 
-  # 🔹 Permisos CloudWatch
+  ######################################
+  # 1. CloudWatch Logs (Scoped)
+  ######################################
   statement {
     sid    = "CloudWatchLogging"
     effect = "Allow"
@@ -195,120 +205,127 @@ data "aws_iam_policy_document" "pipeline_dev_policy_cb_api" {
       "logs:CreateLogStream",
       "logs:PutLogEvents",
       "logs:PutRetentionPolicy",
-      "logs:DeleteLogGroup",
-      "logs:DeleteLogStream"
+      "logs:DeleteLogGroup"
     ]
     resources = [
-      "arn:aws:logs:*:*:log-group:/aws/lambda/${aws_lambda_function.lambda_function.function_name}:*",
-      "arn:aws:logs:*:*:log-group:/aws/kinesisfirehose/${var.firehose_name}:*",
-      "arn:aws:logs:*:*:log-group:/aws/scheduler/${var.scheduler_name}:*"
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${aws_lambda_function.lambda_function.function_name}:*",
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/kinesisfirehose/${var.firehose_name}:*",
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/scheduler/${var.scheduler_name}:*"
     ]
   }
 
-  # 🔹 Permisos S3 + KMS
+  ######################################
+  # 2. S3 Permissions (Data Lake Storage)
+  ######################################
   statement {
-    sid    = "S3K"
+    sid    = "S3AccessForDataLake"
     effect = "Allow"
     actions = [
-      "s3:AbortMultipartUpload",
-      "s3:GetBucketLocation",
-      "s3:GetObject",
       "s3:ListBucket",
-      "s3:ListBucketMultipartUploads",
+      "s3:GetBucketLocation",
       "s3:PutObject",
-      "s3:PutObjectAcl",
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey"
+      "s3:GetObject",
+      "s3:DeleteObject"
     ]
     resources = [
       "arn:aws:s3:::${var.bucket_name}",
-      "arn:aws:s3:::${var.bucket_name}/*",
-      var.kms_key_arn
+      "arn:aws:s3:::${var.bucket_name}/*"
     ]
   }
 
-  # 🔹 Permisos Kinesis (🔥 FIX APLICADO)
-  # Mantenemos tu var.kinesis_stream_arn, pero añadimos wildcard como fallback
+  ######################################
+  # 3. Kinesis Stream & Firehose Integration
+  ######################################
   statement {
-    sid    = "KinesisAccess"
+    sid    = "KinesisFirehoseIntegration"
     effect = "Allow"
     actions = [
+      # Kinesis
       "kinesis:DescribeStream",
-      "kinesis:DescribeStreamSummary",
-      "kinesis:GetRecords",
       "kinesis:GetShardIterator",
+      "kinesis:GetRecords",
       "kinesis:ListShards",
-      "kinesis:ListStreams",
-      "kinesis:PutRecord",
-      "kinesis:PutRecords"
+      # Firehose
+      "firehose:PutRecord",
+      "firehose:PutRecordBatch",
+      "firehose:DescribeDeliveryStream"
     ]
-    # ✅ Incluimos ambos: ARN específico + comodín (sin borrar el tuyo)
     resources = [
-      "arn:aws:kinesis:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:stream/*"
+      "arn:aws:kinesis:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:stream/${var.stream_name}",
+      "arn:aws:firehose:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.firehose_name}"
     ]
   }
 
-  # 🔹 Permisos Glue y LakeFormation
+  ######################################
+  # 4. Glue Catalog & Lake Formation
+  ######################################
   statement {
-    sid    = "GlueAccess"
+    sid    = "GlueAndLakeFormation"
     effect = "Allow"
     actions = [
       "glue:CreateDatabase",
+      "glue:DeleteDatabase",
       "glue:GetDatabase",
       "glue:GetDatabases",
       "glue:CreateTable",
-      "glue:UpdateTable",
+      "glue:DeleteTable",
       "glue:GetTable",
       "glue:GetTables",
-      "glue:DeleteTable",
+      "glue:UpdateTable",
       "glue:GetPartition",
       "glue:GetPartitions",
       "glue:BatchCreatePartition",
       "glue:BatchDeletePartition",
       "glue:BatchGetPartition",
-      "glue:GetCrawler",
-      "glue:GetCrawlers",
-      "glue:StartCrawler",
-      "glue:UpdateCrawler",
-      "glue:UpdatePartition",
-      "glue:CreateCrawler",
-      "glue:GetCatalogImportStatus",
-      "lakeformation:GetDataAccess",
       "lakeformation:GrantPermissions",
-      "lakeformation:RevokePermissions",
-      "lakeformation:ListPermissions",
-      "lakeformation:GetEffectivePermissionsForPrincipal",
+      "lakeformation:GetDataAccess",
       "lakeformation:GetResourceLFTags",
-      "lakeformation:ListLFTags",
-      "lakeformation:GetLFTag",
-      "lakeformation:SearchDatabasesByLFTags",
-      "lakeformation:SearchTablesByLFTags"
+      "lakeformation:AddLFTagsToResource",
+      "lakeformation:ListPermissions",
+      "lakeformation:RevokePermissions"
     ]
     resources = [
       "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog",
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:database/${var.database}",
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${var.database}/*",
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:crawler/${var.crawler}",
-      "arn:aws:lakeformation:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog:${data.aws_caller_identity.current.account_id}"
+      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:database/*",
+      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/*/*",
+      "arn:aws:lakeformation:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog"
     ]
+  }
+
+  ######################################
+  # 5. KMS Access (Encryption for S3, Glue)
+  ######################################
+  statement {
+    sid    = "KMSAccess"
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:GenerateDataKey",
+      "kms:DescribeKey"
+    ]
+    resources = [var.kms_key_arn]
+  }
+
+  ######################################
+  # 6. EventBridge / Scheduler Invocation
+  ######################################
+  statement {
+    sid    = "AllowEventBridgeInvokeLambda"
+    effect = "Allow"
+    actions = [
+      "lambda:InvokeFunction"
+    ]
+    resources = ["*"]
   }
 }
 
-# 🔹 Attach Inline Policy al Role
-resource "aws_iam_role_policy" "lambda_permissions" {
-  name   = "lambda_logging_with_layer"
-  role   = aws_iam_role.iam_dev_role_cb_api.name
-  policy = data.aws_iam_policy_document.pipeline_dev_policy_cb_api.json
-}
+#################################################
+# IAM ROLE POLICY ATTACHMENT
+#################################################
 
-# 🔹 Permitir invocación desde EventBridge Scheduler
-resource "aws_lambda_permission" "allow_scheduler_invoke" {
-  statement_id  = "AllowEventBridgeInvokeLambda"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_function.function_name
-  principal     = "scheduler.amazonaws.com"
-  source_arn    = var.scheduler_arn
+resource "aws_iam_role_policy" "pipeline_dev_policy_cb_api" {
+  name   = "pipeline_dev_policy_cb_api"
+  role   = aws_iam_role.iam_dev_role_cb_api.id
+  policy = data.aws_iam_policy_document.pipeline_dev_policy_cb_api.json
 }
