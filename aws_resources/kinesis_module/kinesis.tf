6abff1c4d545da6e107b46b2956f878bfb982194
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-30 19:24:30 -0500

    convering file to json

--- aws_resources/kinesis_module/kinesis.tf
+++ aws_resources/kinesis_module/kinesis.tf
@@ -11,9 +11,26 @@ resource "aws_kinesis_stream" "coinbase_stream" {
   }
 }
 
+# resource "aws_kinesis_firehose_delivery_stream" "coinbase_firehose" {
+#   name        = "coinbase-firehose"
+#   destination = "extended_s3"
+
+#   extended_s3_configuration {
+#     role_arn           = var.lambda_role
+#     bucket_arn         = var.bucket_arn
+#     kms_key_arn        = var.kms_key_arn
+#   }
+
+#   kinesis_source_configuration {
+#     kinesis_stream_arn = aws_kinesis_stream.coinbase_stream.arn
+#     role_arn           = var.lambda_role
+#   }
+# }
+
+
 resource "aws_kinesis_firehose_delivery_stream" "coinbase_firehose" {
   name        = "coinbase-firehose"
-  destination = "extended_s3"
+  destination = "s3"
 
   extended_s3_configuration {
     role_arn           = var.lambda_role
@@ -21,10 +38,24 @@ resource "aws_kinesis_firehose_delivery_stream" "coinbase_firehose" {
     kms_key_arn        = var.kms_key_arn
   }
 
+  data_format_conversion_configuration {
+    enabled = true
+
+    input_format_configuration {
+      deserializer {
+        open_x_json_ser_de {}
+      }
+    }
+
+    output_format_configuration {
+      serializer {
+        open_x_json_ser_de {}
+      }
+    }
+  }
+
   kinesis_source_configuration {
     kinesis_stream_arn = aws_kinesis_stream.coinbase_stream.arn
     role_arn           = var.lambda_role
   }
 }
-
-
