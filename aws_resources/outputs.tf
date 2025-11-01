6d4fe3bf9187defa7c1d7f74d4d2f1f9ef34d871
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-11-01 12:13:27 -0500

    adding glue crawler steps

--- aws_resources/outputs.tf
+++ aws_resources/outputs.tf
@@ -2,3 +2,8 @@ output "athena_workgroup_name" {
   description = "Athena workgroup name exposed by the Athena module"
   value       = module.bucket_utils.athena_workgroup
 }
+
+output "glue_crawler" {
+  description = "Glue Crawler exposed by the glue catalog module"
+  value       = module.glue_catalog_utils.crawler
+}
