8895c8cb02fe1059db7daf596d5caa260d30d219
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-31 19:50:17 -0500

    Update catalog.tf

--- aws_resources/glue_catalog_module/catalog.tf
+++ aws_resources/glue_catalog_module/catalog.tf
@@ -7,18 +7,39 @@ data "aws_region" "current" {}
 ##########################################
 # 🧩 1️⃣ Crear el rol de Glue
 ##########################################
+# resource "aws_iam_role" "glue_role" {
+#   name = "iam_glue_crawler_role"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [{
+#       Effect = "Allow",
+#       Principal = { Service = "glue.amazonaws.com","lakeformation.amazonaws.com" },
+#       Action    = "sts:AssumeRole"
+#     }]
+#   })
+# }
+
 resource "aws_iam_role" "glue_role" {
   name = "iam_glue_crawler_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = { Service = "glue.amazonaws.com" },
-      Action    = "sts:AssumeRole"
-    }]
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = [
+            "glue.amazonaws.com",
+            "lakeformation.amazonaws.com"
+          ]
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
   })
 }
+
 
 resource "aws_iam_role_policy_attachment" "glue_service_role_attach" {
   role       = aws_iam_role.glue_role.name
