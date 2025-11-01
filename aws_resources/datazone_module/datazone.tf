48d0767c29fbc97884859b300462ec9d9d5bb1df
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-11-01 00:17:34 -0500

    Update datazone.tf

--- aws_resources/datazone_module/datazone.tf
+++ aws_resources/datazone_module/datazone.tf
@@ -123,4 +123,7 @@ resource "aws_datazone_project" "analytics_team" {
   domain_identifier = aws_datazone_domain.coinbase_domain.id
   name              = "analytics-project"
   description       = "BI & analytics team project for Coinbase Lake data"
+  project_owners = [
+    "arn:aws:iam::163257074638:user/rroatest"
+  ]
 }
